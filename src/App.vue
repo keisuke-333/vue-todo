186c3eec9346e4c4fb5c7415b4bb44f62bbbeaff
keisuke-333 <keikubo.999@gmail.com>
2020-10-27 21:49:11 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <form class="form" v-on:submit.prevent>
+      <div class="inputArea">
+        <input type="text" class="inputText" placeholder="Please enter" v-model="newTodoName" v-on:keydown.enter="addTodo">
+      </div>
+    </form>
+
+    <ul class="list">
+      <li class="list__item" v-for="(todo, key) in todos" :key="key" :class="{ done : todo.isComplete}">
+        <span v-on:click="doneTodo(todo, key)">
+          <i class="far fa-square icon-square" v-if="!todo.isComplete"></i>
+          <i class="far fa-check-square icon-check" v-else></i>
+        </span>
+        <span class="task">{{ todo.name }}</span>
+        <i class="fas fa-times icon-times" v-on:click="removeTodo(key)"></i>
+      </li>
+    </ul>
+
+
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import firebase from "firebase/app"
+import "firebase/firestore"
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      newTodoName: '',
+      db: null,
+      todosRef: null,
+      todos: {}
+    }
+  },
+  created: function() {
+    this.db = firebase.firestore()
+    this.todosRef = this.db.collection('todo')
+    this.todosRef.onSnapshot(querySnapshot => {
+      const obj = {}
+      querySnapshot.forEach(doc => {
+        obj[doc.id] = doc.data()
+      })
+      this.todos = obj
+    })
+  },
+  methods: {
+    addTodo: function(e) {
+      if(e.keyCode !== 13) return;
+      if(this.newTodoName == '') return;
+      this.todosRef.add({
+        name: this.newTodoName,
+        isComplete: false
+      });
+      this.newTodoName = '';
+    },
+    doneTodo: function(todo, key) {
+      todo.isComplete = !todo.isComplete
+      this.todosRef.doc(key).update(todo)
+    },
+    removeTodo: function(key) {
+      this.todosRef.doc(key).delete()
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import firebase from "firebase/app"
+import "firebase/firestore"
+import './assets/css/global.css'
 
 Vue.config.productionTip = false
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCSyO_jUkOKyUB4Xr5XjvW5uE75uPTAVkc",
+  authDomain: "vue-todo-97f6a.firebaseapp.com",
+  databaseURL: "https://vue-todo-97f6a.firebaseio.com",
+  projectId: "vue-todo-97f6a",
+  storageBucket: "vue-todo-97f6a.appspot.com",
+  messagingSenderId: "879354516264",
+  appId: "1:879354516264:web:1f18c0f4e1468ca193761d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   render: h => h(App),
